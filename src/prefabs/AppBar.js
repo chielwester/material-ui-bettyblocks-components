a15133c593bfc744b219da6df88db665e6f2c4a7
Chiel Wester <chiel.wester@bettyblocks.com>
2020-01-03 17:09:08 +0100

    Add the AppBar component

---
+++ src/prefabs/AppBar.js
@@ -0,0 +1,19 @@
+(() => ({
+  name: 'AppBar',
+  icon: 'NavbarIcon',
+  category: 'MATERIAL',
+  structure: [
+    {
+      name: 'AppBar',
+      options: [
+      	{
+      		label: 'Title',
+					key: 'title',
+					value: ['App Bar'],
+					type: 'VARIABLE'
+      	}
+      ],
+      descendants: [],
+    },
+  ],
+}))();
