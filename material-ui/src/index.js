58fffce75b3aab42f7f920f75576f06ee074f791
Stephan Kaag <stephan@ka.ag>
2020-01-08 14:12:04 +0100

    Add @material-ui/lab

--- material-ui/src/index.js
+++ material-ui/src/index.js
@@ -1,2 +1,9 @@
-import * as MaterialUI from "@material-ui/core"; 
-window.MaterialUI = MaterialUI
+import * as Core from '@material-ui/core';
+import * as Lab from '@material-ui/lab';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+window.MaterialUI = {
+  Core,
+  Lab,
+  Styles: { ThemeProvider, createMuiTheme },
+};
