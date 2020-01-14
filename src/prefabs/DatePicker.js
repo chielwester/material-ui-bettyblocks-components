95c3b779cddcb75627f3df4afac3a6747546b594
Stephan Kaag <stephan@ka.ag>
2020-01-14 14:09:55 +0100

    Move datepicker to form category

--- src/prefabs/DatePicker.js
+++ src/prefabs/DatePicker.js
@@ -1,21 +1,21 @@
 (() => ({
   name: 'DatePicker',
   icon: 'DatePickerIcon',
-  category: 'MATERIAL',
+  category: 'FORM',
   structure: [
     {
       name: 'DatePicker',
       options: [
-				{
-				  type: 'TEXT',
-				  label: 'Label',
-				  key: 'label',
-				  value: 'Material Datepicker',
-				},
+        {
+          type: 'TEXT',
+          label: 'Label',
+          key: 'label',
+          value: 'Material Datepicker',
+        },
         {
           value: '',
           label: 'Name',
-          key: 'formComponentName',
+          key: 'name',
           type: 'TEXT',
         },
         {
@@ -30,76 +30,76 @@
           key: 'disableToolbar',
           type: 'TOGGLE',
         },
-				{
-				  type: 'CUSTOM',
-				  label: 'Variant',
-				  key: 'variant',
-				  value: 'normal',
-				  configuration: {
-				    as: 'BUTTONGROUP',
-				    dataType: 'string',
-				    allowedInput: [
-				      {
-				        name: 'Normal',
-				        value: 'normal'
-				      },
-				      {
-				        name: 'Inline',
-				        value: 'inline'
-				      },
-				    ]
-				  }
-				},
-				{
-				  type: 'VARIABLE',
-				  label: 'Default Value',
-				  key: 'defaultValue',
-				  value: [''],
-				},
-				{
-				  type: 'CUSTOM',
-				  label: 'Margin',
-				  key: 'margin',
-				  value: 'none',
-				  configuration: {
-				    as: 'BUTTONGROUP',
-				    dataType: 'string',
-				    allowedInput: [
-				      {
-				        name: 'None',
-				        value: 'none'
-				      },
-				      {
-				        name: 'Dense',
-				        value: 'dense'
-				      },
-				      {
-				        name: 'Normal',
-				        value: 'normal'
-				      }
-				    ]
-				  }
-				},
-				{
-				  type: 'CUSTOM',
-				  label: 'Date format',
-				  key: 'dateformat',
-				  value: 'MM/dd/yyyy',
-				  configuration: {
-				    as: 'BUTTONGROUP',
-				    dataType: 'string',
-				    allowedInput: [
-				      {
-				        name: 'MM/dd/yyyy',
-				        value: 'MM/dd/yyyy'
-				      },
-				      {
-				        name: 'dd/MM/yyyy',
-				        value: 'dd/MM/yyyy'
-				      },
-				    ]
-				  }
-				}
+        {
+          type: 'CUSTOM',
+          label: 'Variant',
+          key: 'variant',
+          value: 'normal',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'Normal',
+                value: 'normal',
+              },
+              {
+                name: 'Inline',
+                value: 'inline',
+              },
+            ],
+          },
+        },
+        {
+          type: 'VARIABLE',
+          label: 'Default Value',
+          key: 'defaultValue',
+          value: [''],
+        },
+        {
+          type: 'CUSTOM',
+          label: 'Margin',
+          key: 'margin',
+          value: 'none',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'None',
+                value: 'none',
+              },
+              {
+                name: 'Dense',
+                value: 'dense',
+              },
+              {
+                name: 'Normal',
+                value: 'normal',
+              },
+            ],
+          },
+        },
+        {
+          type: 'CUSTOM',
+          label: 'Date format',
+          key: 'dateformat',
+          value: 'MM/dd/yyyy',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'MM/dd/yyyy',
+                value: 'MM/dd/yyyy',
+              },
+              {
+                name: 'dd/MM/yyyy',
+                value: 'dd/MM/yyyy',
+              },
+            ],
+          },
+        },
       ],
       descendants: [],
     },
