0bdf57158bca21cf97c0c448d391db1899d85374
Chiel Wester <chiel.wester@bettyblocks.com>
2020-01-14 09:41:14 +0100

    Add DatePicker Component

--- material-ui/src/index.js
+++ material-ui/src/index.js
@@ -1,9 +1,15 @@
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
 import * as Core from '@material-ui/core';
 import * as Lab from '@material-ui/lab';
+import * as Pickers from '@material-ui/pickers';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
+
 window.MaterialUI = {
   Core,
   Lab,
+	DateFnsUtils,
+	Pickers,
   Styles: { ThemeProvider, createMuiTheme },
 };
