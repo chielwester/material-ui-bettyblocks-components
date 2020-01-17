822521501688991aa864c468886b71d15f17527a
Stephan Kaag <stephan@ka.ag>
2020-01-17 11:00:26 +0100

    Add box component

---
+++ src/components/Box.js
@@ -0,0 +1,66 @@
+(() => ({
+  name: 'Box',
+  type: 'BOX',
+  allowedTypes: [
+    'ACCORDION',
+    'ALERT',
+    'BREADCRUMBS',
+    'BUTTON',
+    'BOX',
+    'APP_BAR',
+    'CARD',
+    'CHIP',
+    'DIVIDER',
+    'DRAWER',
+    'FORM',
+    'IMAGE',
+    'LIST',
+    'TOOLTIP',
+    'PAGE_BODY',
+    'PAPER',
+    'PROGRESS',
+    'SNACKBAR',
+    'STEPPER',
+    'TABLE',
+    'TABS',
+    'TEXT',
+  ],
+  orientation: 'HORIZONTAL',
+  jsx: (() => {
+    const { Box } = window.MaterialUI.Core;
+    const isDev = B.env === 'dev';
+    const isPristine = !children.length && isDev;
+    const toSpaceProp = value => {
+      if (!value || !value.length) return false;
+      return isNaN(value) ? value : +value;
+    };
+    const spacingProps = Object.keys(options).reduce((map, option) => {
+      const value = toSpaceProp(options[option]);
+      return value === false ? map : Object.assign(map, { [option]: value });
+    }, {});
+    const box = (
+      <Box
+        className={[
+          isPristine ? classes.pristine : '',
+          isDev ? classes.dev : '',
+        ].join(' ')}
+        {...spacingProps}
+      >
+        {children}
+      </Box>
+    );
+    return isDev ? <div>{box}</div> : box;
+  })(),
+  styles: () => () => ({
+    dev: {
+      paddingBottom: '10px',
+    },
+    pristine: {
+      borderWidth: '0.0625rem',
+      borderColor: '#AFB5C8',
+      borderStyle: 'dashed',
+      backgroundColor: '#F0F1F5',
+      height: '100px',
+    },
+  }),
+}))();
