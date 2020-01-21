c2b406dfe5af29f032395ffc6462d16e885b3372
Justin <justinvdv@live.nl>
2020-01-21 10:33:31 +0100

    Fixed formating

--- src/prefabs/Button.js
+++ src/prefabs/Button.js
@@ -2,175 +2,183 @@
   name: 'Button',
   icon: 'ButtonIcon',
   category: 'CONTENT',
-  structure: [{
-    name: 'Button',
-    options: [{
-        type: 'VARIABLE',
-        label: 'Button text',
-        key: 'buttonText',
-        value: ['Button'],
-      },
-      {
-        label: 'StartIcon',
-        key: 'startIcon',
-        value: 'None',
-        type: 'CUSTOM',
-        configuration: {
-          as: 'DROPDOWN',
-          dataType: 'string',
-          allowedInput: [{
-              name: '',
-              value: 'None',
-            },
-            {
-              name: 'ExpandMore',
-              value: 'ExpandMore',
-            },
-            {
-              name: 'FilterList',
-              value: 'FilterList',
-            },
-            {
-              name: 'Search',
-              value: 'Search',
-            },
-            {
-              name: 'FileCopy',
-              value: 'FileCopy',
-            },
-            {
-              name: 'GetApp',
-              value: 'GetApp',
-            },
-            {
-              name: 'Email',
-              value: 'Email',
-            },
-          ],
+  structure: [
+    {
+      name: 'Button',
+      options: [
+        {
+          type: 'VARIABLE',
+          label: 'Button text',
+          key: 'buttonText',
+          value: ['Button'],
         },
-      },
-      {
-        type: 'CUSTOM',
-        label: 'variant',
-        key: 'variant',
-        value: 'contained',
-        configuration: {
-          as: 'BUTTONGROUP',
-          dataType: 'string',
-          allowedInput: [{
-              name: 'Text',
-              value: 'text'
-            },
-            {
-              name: 'Outlined',
-              value: 'outlined'
-            },
-            {
-              name: 'Contained',
-              value: 'contained'
-            },
-          ],
-        },
-      },
-      {
-        label: 'Color',
-        key: 'color',
-        value: 'default',
-        type: 'CUSTOM',
-        configuration: {
-          as: 'BUTTONGROUP',
-          dataType: 'string',
-          allowedInput: [{
-              name: 'Default',
-              value: 'default'
-            },
-            {
-              name: 'Inherit',
-              value: 'inherit'
-            },
-            {
-              name: 'Primary',
-              value: 'primary'
-            },
-            {
-              name: 'Secondary',
-              value: 'secondary'
-            },
-          ],
-        },
-      },
-      {
-        type: 'CUSTOM',
-        label: 'Link to',
-        key: 'linkType',
-        value: 'Internal',
-        configuration: {
-          as: 'BUTTONGROUP',
-          dataType: 'string',
-          allowedInput: [{
-              name: 'Internal page',
-              value: 'Internal'
-            },
-            {
-              name: 'External page',
-              value: 'External'
-            },
-          ],
-        },
-      },
-      {
-        value: '',
-        label: 'Page',
-        key: 'linkTo',
-        type: 'ENDPOINT',
-        configuration: {
-          condition: {
-            type: 'SHOW',
-            option: 'linkType',
-            comparator: 'EQ',
-            value: 'Internal',
+        {
+          label: 'StartIcon',
+          key: 'startIcon',
+          value: 'None',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'DROPDOWN',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: '',
+                value: 'None',
+              },
+              {
+                name: 'ExpandMore',
+                value: 'ExpandMore',
+              },
+              {
+                name: 'FilterList',
+                value: 'FilterList',
+              },
+              {
+                name: 'Search',
+                value: 'Search',
+              },
+              {
+                name: 'FileCopy',
+                value: 'FileCopy',
+              },
+              {
+                name: 'GetApp',
+                value: 'GetApp',
+              },
+              {
+                name: 'Email',
+                value: 'Email',
+              },
+            ],
           },
         },
-      },
-      {
-        value: '',
-        label: 'URL',
-        key: 'linkToExternal',
-        type: 'TEXT',
-        configuration: {
-          placeholder: 'Starts with https:// or http://',
-          condition: {
-            type: 'SHOW',
-            option: 'linkType',
-            comparator: 'EQ',
-            value: 'External',
+        {
+          type: 'CUSTOM',
+          label: 'variant',
+          key: 'variant',
+          value: 'contained',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'Text',
+                value: 'text',
+              },
+              {
+                name: 'Outlined',
+                value: 'outlined',
+              },
+              {
+                name: 'Contained',
+                value: 'contained',
+              },
+            ],
           },
         },
-      },
-      {
-        label: 'Size',
-        key: 'size',
-        value: 'medium',
-        type: 'CUSTOM',
-        configuration: {
-          as: 'BUTTONGROUP',
-          dataType: 'string',
-          allowedInput: [{
-              name: 'Small',
-              value: 'small'
-            },
-            {
-              name: 'Medium',
-              value: 'medium'
-            },
-            {
-              name: 'Large',
-              value: 'large'
-            },
-          ],
+        {
+          label: 'Color',
+          key: 'color',
+          value: 'default',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'Default',
+                value: 'default',
+              },
+              {
+                name: 'Inherit',
+                value: 'inherit',
+              },
+              {
+                name: 'Primary',
+                value: 'primary',
+              },
+              {
+                name: 'Secondary',
+                value: 'secondary',
+              },
+            ],
+          },
         },
-      },
-    ],
-    descendants: [],
-  }, ],
+        {
+          type: 'CUSTOM',
+          label: 'Link to',
+          key: 'linkType',
+          value: 'Internal',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'Internal page',
+                value: 'Internal',
+              },
+              {
+                name: 'External page',
+                value: 'External',
+              },
+            ],
+          },
+        },
+        {
+          value: '',
+          label: 'Page',
+          key: 'linkTo',
+          type: 'ENDPOINT',
+          configuration: {
+            condition: {
+              type: 'SHOW',
+              option: 'linkType',
+              comparator: 'EQ',
+              value: 'Internal',
+            },
+          },
+        },
+        {
+          value: '',
+          label: 'URL',
+          key: 'linkToExternal',
+          type: 'TEXT',
+          configuration: {
+            placeholder: 'Starts with https:// or http://',
+            condition: {
+              type: 'SHOW',
+              option: 'linkType',
+              comparator: 'EQ',
+              value: 'External',
+            },
+          },
+        },
+        {
+          label: 'Size',
+          key: 'size',
+          value: 'medium',
+          type: 'CUSTOM',
+          configuration: {
+            as: 'BUTTONGROUP',
+            dataType: 'string',
+            allowedInput: [
+              {
+                name: 'Small',
+                value: 'small',
+              },
+              {
+                name: 'Medium',
+                value: 'medium',
+              },
+              {
+                name: 'Large',
+                value: 'large',
+              },
+            ],
+          },
+        },
+      ],
+      descendants: [],
+    },
+  ],
 }))();
