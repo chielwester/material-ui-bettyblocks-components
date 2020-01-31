0a694a4be8a326b889947218fc203648f57fbddb
Emiel Wit <emielwit@MacBook-Pro-van-Frits.local>
2020-01-31 16:52:39 +0100

    add react-dropzone

--- material-ui/src/index.js
+++ material-ui/src/index.js
@@ -3,6 +3,7 @@ import DateFnsUtils from '@date-io/date-fns';
 import * as Core from '@material-ui/core';
 import * as Lab from '@material-ui/lab';
 import * as Pickers from '@material-ui/pickers';
+import Dropzone from 'react-dropzone'
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import {
@@ -20,5 +21,6 @@ window.MaterialUI = {
   DateFnsUtils,
   Pickers,
   Styles: { ThemeProvider, createMuiTheme },
-  Icons: { ExpandMore, FilterList, Search, FileCopy, Email, GetApp }
+  Icons: { ExpandMore, FilterList, Search, FileCopy, Email, GetApp },
+  Dropzone
 };
--- src/components/Dropzone.js
+++ src/components/Dropzone.js
@@ -4,19 +4,26 @@
   allowedTypes: [],
   orientation: 'HORIZONTAL',
   jsx: (() => {
-    const { DropzoneArea } = window.MaterialUI;
+    const { Dropzone } = window.MaterialUI;
     const isDev = B.env === 'dev';
     const [files, setFiles] = useState([]);
-    const handleChange = files => {
+    const handleChange = (e, file) => {
       setFiles({
         files: files,
       });
     };
 
     const dropzone = (
-      <DropzoneArea
-        onChange={handleChange}
-      />
+      <Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>
+        {({ getRootProps, getInputProps }) => (
+          <section>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+          </section>
+        )}
+      </Dropzone>
     );
     return isDev ? <div>{dropzone}</div> : dropzone;
   })(),
