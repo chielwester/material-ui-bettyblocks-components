de6390c4922a16b4cbb006116174d72027533a66
Emiel Wit <emielwit@MacBook-Pro-van-Frits.local>
2020-02-05 11:35:09 +0100

    change radiogroup order

--- src/components/RadioGroupInput.js
+++ src/components/RadioGroupInput.js
@@ -32,71 +32,83 @@
       FormLabel,
       Radio,
     } = window.MaterialUI.Core;
-    const radiogroup = (
-      <RadioGroup
-        row={row}
-        value={value}
-        name={name}
-        onChange={event => setValue(event.target.value)}
-        helperText={helperText}
-      >
-        {optionType === 'static' ? (
-          (selectOptions || '')
-            .split('\n')
-            .map(option => (
-              <FormControlLabel
-                disabled={disabled}
-                value={option}
-                control={<Radio color={color} />}
-                label={option}
-              />
-            ))
-        ) : !isDev ? (
-          <GetAll modelId={model} skip={0} take={15}>
-            {({ loading, error, data }) => {
-              if (loading) {
-                return <span>Loading...</span>;
-              }
-
-              if (error) {
-                return <span>Something went wrong: {error.message} :(</span>;
-              }
-
-              const { results } = data;
-
-              return results.map(item => (
-                <FormControlLabel
-                  disabled={disabled}
-                  value={item.id}
-                  control={<Radio color={color} />}
-                  label={item[labelProperty.name]}
-                />
-              ));
-            }}
-          </GetAll>
-        ) : (
-          <>
+    const radiogroup =
+      optionType === 'static' ? (
+        <RadioGroup
+          row={row}
+          value={value}
+          name={name}
+          onChange={event => setValue(event.target.value)}
+          helperText={helperText}
+        >
+          {(selectOptions || '').split('\n').map(option => (
             <FormControlLabel
               disabled={disabled}
-              value="placeholder1"
+              value={option}
               control={<Radio color={color} />}
-              label="Placeholder"
+              label={option}
             />
-            <FormControlLabel
-              disabled={disabled}
-              value="placeholder2"
-              control={<Radio color={color} />}
-              label="Placeholder"
-            />
-          </>
-        )}
-      </RadioGroup>
-    );
+          ))}
+        </RadioGroup>
+      ) : !isDev ? (
+        <GetAll modelId={model} skip={0} take={50}>
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <span>Loading...</span>;
+            }
+
+            if (error) {
+              return <span>Something went wrong: {error.message} :(</span>;
+            }
+
+            const { results } = data;
+            return (
+              <RadioGroup
+                row={row}
+                value={value}
+                name={name}
+                onChange={event => setValue(event.target.value)}
+                helperText={helperText}
+              >
+                {results.map(item => (
+                  <FormControlLabel
+                    disabled={disabled}
+                    value={item.id}
+                    control={<Radio color={color} />}
+                    label={item[labelProperty.name]}
+                  />
+                ))}
+              </RadioGroup>
+            );
+          }}
+        </GetAll>
+      ) : (
+        <RadioGroup
+          row={row}
+          value={value}
+          name={name}
+          onChange={event => setValue(event.target.value)}
+          helperText={helperText}
+        >
+          <FormControlLabel
+            disabled={disabled}
+            value="placeholder1"
+            control={<Radio color={color} />}
+            label="Placeholder"
+          />
+          <FormControlLabel
+            disabled={disabled}
+            value="placeholder2"
+            control={<Radio color={color} />}
+            label="Placeholder"
+          />
+        </RadioGroup>
+      );
 
     const formControl = (
       <FormControl className={classes.radioButtonGroup} component="fieldset">
         <FormLabel component="legend">{label}</FormLabel>
-        {radiogroup}
+          {radiogroup}
         <FormHelperText>{helperText}</FormHelperText>
       </FormControl>
     );
