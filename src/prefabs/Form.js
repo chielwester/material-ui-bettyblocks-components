8ae230686f9d9cdb4982be15833422f13d2c0218
Stephan Kaag <stephan@ka.ag>
2020-03-03 10:29:39 +0100

    Remove obsolete form options

--- src/prefabs/Form.js
+++ src/prefabs/Form.js
@@ -8,24 +8,6 @@
       options: [
         {
           value: '',
-          label: 'Post endpoint',
-          key: 'resourceUrl',
-          type: 'TEXT',
-        },
-        {
-          value: 'Thanks for submitting the form!',
-          label: 'Success message',
-          key: 'formSuccessMessage',
-          type: 'TEXT',
-        },
-        {
-          value: 'Failed to submit the form!',
-          label: 'Error message',
-          key: 'formErrorMessage',
-          type: 'TEXT',
-        },
-        {
-          value: '',
           label: 'Action',
           key: 'actionId',
           type: 'ACTION',
@@ -50,92 +32,7 @@
         },
       ],
 
-      descendants: [
-        // {
-        //   name: 'TextInput',
-        //   options: [
-        //     {
-        //       value: '',
-        //       label: 'Name',
-        //       key: 'formComponentName',
-        //       type: 'TEXT',
-        //     },
-        //     {
-        //       value: 'Label text',
-        //       label: 'Label text',
-        //       key: 'formComponentLabel',
-        //       type: 'TEXT',
-        //     },
-        //     {
-        //       value: '',
-        //       label: 'Input text',
-        //       key: 'formComponentValue',
-        //       type: 'TEXT',
-        //     },
-        //     {
-        //       value: false,
-        //       label: 'Required',
-        //       key: 'formComponentRequired',
-        //       type: 'TOGGLE',
-        //     },
-        //   ],
-        //   descendants: [],
-        // },
-        // {
-        //   name: 'TextInput',
-        //   options: [
-        //     {
-        //       value: '',
-        //       label: 'Name',
-        //       key: 'formComponentName',
-        //       type: 'TEXT',
-        //     },
-        //     {
-        //       value: 'Label text',
-        //       label: 'Label text',
-        //       key: 'formComponentLabel',
-        //       type: 'TEXT',
-        //     },
-        //     {
-        //       value: '',
-        //       label: 'Input text',
-        //       key: 'formComponentValue',
-        //       type: 'TEXT',
-        //     },
-        //     {
-        //       value: false,
-        //       label: 'Required',
-        //       key: 'formComponentRequired',
-        //       type: 'TOGGLE',
-        //     },
-        //   ],
-        //   descendants: [],
-        // },
-        // {
-        //   name: 'SubmitButton',
-        //   options: [
-        //     {
-        //       value: 'Submit',
-        //       label: 'Button text',
-        //       key: 'submitButtonText',
-        //       type: 'TEXT',
-        //     },
-        //     {
-        //       value: 'Primary',
-        //       label: 'Button Color',
-        //       key: 'backgroundColor',
-        //       type: 'COLOR',
-        //     },
-        //     {
-        //       value: ['0rem', 'M', '0rem', '0rem'],
-        //       label: 'Outer space',
-        //       key: 'outerSpacing',
-        //       type: 'SIZES',
-        //     },
-        //   ],
-        //   descendants: [],
-        // },
-      ],
+      descendants: [],
     },
   ],
 }))();
